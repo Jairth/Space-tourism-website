f829e375ead1a87fb5b04757e943f0a68cf0c4e1
Jairth <rodriguez6191@hotmail.com>
2023-03-22 22:35:44 -0500

    First commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { DestinationComponent } from './destination/destination.component';
+import { CrewComponent } from './crew/crew.component';
+import { TechnologyComponent } from './technology/technology.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DestinationComponent,
+    CrewComponent,
+    TechnologyComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomeComponent,
+    DestinationComponent,
+    CrewComponent,
+    TechnologyComponent
+  ]
+})
+export class PagesModule { }
