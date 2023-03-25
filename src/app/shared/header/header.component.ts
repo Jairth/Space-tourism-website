9671d01e4500c6ba23e05e887eae01057e6978e0
Jairth <rodriguez6191@hotmail.com>
2023-03-25 13:35:09 -0500

    home

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -4,30 +4,34 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  public active:boolean = false
+  public active: boolean = false;
 
-  constructor(private router:Router){}
+  constructor(private router: Router) {}
 
   change() {
-    this.active = !this.active
+    this.active = !this.active;
   }
 
   destination() {
-    this.router.navigate(['/destination'])
+    this.router.navigate(['/destination']);
+    this.active = false;
   }
 
   navigateToHome() {
     this.router.navigate(['/']);
+    this.active = false;
   }
 
-  crew(){
-    this.router.navigate(['/crew'])
+  crew() {
+    this.router.navigate(['/crew']);
+    this.active = false;
   }
 
   technology() {
-    this.router.navigate(['/technology'])
+    this.router.navigate(['/technology']);
+    this.active = false;
   }
 }
