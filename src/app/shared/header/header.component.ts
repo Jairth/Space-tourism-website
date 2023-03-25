f63d842f0c3ad6b01c5e671a052199b0f62fe5cc
Jairth <rodriguez6191@hotmail.com>
2023-03-24 22:11:06 -0500

    Change animation navbar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,25 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   public active:boolean = false
 
+  constructor(private router:Router){}
+
   change() {
     this.active = !this.active
+  }
+
+  destination() {
+    this.router.navigate(['/destination'])
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/']);
+  }
+
+  crew(){
+    this.router.navigate(['/crew'])
+  }
+
+  technology() {
+    this.router.navigate(['/technology'])
   }
 }
