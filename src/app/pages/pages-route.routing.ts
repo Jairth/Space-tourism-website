218065cc2e6addcb90bcdd2aaffc7a6563c5c205
Jairth <rodriguez6191@hotmail.com>
2023-03-30 17:57:23 -0500

    carpet dist

--- src/app/pages/pages-route.routing.ts
+++ src/app/pages/pages-route.routing.ts
@@ -5,16 +5,16 @@ import { MoonComponent } from './destination/moon/moon.component';
 import { TitanComponent } from './destination/titan/titan.component';
 
 const routes: Routes = [
-  {
-    path:'',
-    children: [
-      { path: 'moon', component: MoonComponent },
-      { path: 'mars', component: MarsComponent },
-      { path: 'europa', component: EuropaComponent },
-      { path: 'titan', component: TitanComponent },
-      { path: '**', redirectTo: 'moon', pathMatch: 'full' },
-    ],
-  },
+  // {
+  //   path:'',
+  //   children: [
+  //     { path: 'moon', component: MoonComponent },
+  //     { path: 'mars', component: MarsComponent },
+  //     { path: 'europa', component: EuropaComponent },
+  //     { path: 'titan', component: TitanComponent },
+  //     { path: '**', redirectTo: 'moon', pathMatch: 'full' },
+  //   ],
+  // },
 ];
 
 export const PagesRouteRoutes = RouterModule.forChild(routes);
