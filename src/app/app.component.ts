9c391439d05a0f343cef826d6ff9118423da2550
Jairth <rodriguez6191@hotmail.com>
2023-04-02 23:22:22 -0500

    Implement spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spaceApp';
+
+  constructor(private spinner: NgxSpinnerService){}
+
+  ngOnInit() {
+    this.spinner.show(undefined, {
+      type: 'ball-spin',
+      size: 'medium',
+      bdColor: ' #0c0f21',
+      color: '#fff',
+      fullScreen: true,
+    });
+
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 1000);
+  }
 }
